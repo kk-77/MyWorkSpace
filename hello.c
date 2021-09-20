c237501a80bd9cfcc8b331f7ea814d1ed420ab52
kk-77 <1223350405@qq.com>
2021-09-20 17:11:01 +0800

    提交hello.c

--- hello.c
+++ hello.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#define CCM_CCGR1_BASE				(0X020C406C)	
+#define REGISTER_LENGTH				4
+
 int main(){
     
     char name[10];
     printf("Hello Git!\r\n");
-    gets(name);
+    printf("%x\r\n",CCM_CCGR1_BASE + REGISTER_LENGTH - 1);
+   // gets(name);
     return 0;
 }
 
